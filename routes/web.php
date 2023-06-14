d214630341928d1cddbe3751f1c77cf0950b4e84
Dipesh verma <dipeshv20@gmail.com>
2023-06-14 17:22:55 +0530

    Updates

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/',function (){
+    dd('app is working!');
+});
 Route::get('/test-api-call',[\App\Http\Controllers\HubApiController::class,'processApi']);
 Route::get('/register-webhook',[\App\Http\Controllers\HubApiController::class,'registerWebhooks']);
 Route::any('/subscription-webhook-created',[\App\Http\Controllers\HubApiController::class,'subscriptionCreated']);
